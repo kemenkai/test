15bfb6ce5e6dfdbb8ce721df37a56bb1d51b33be
kemenkai <282913448@qq.com>
2018-01-12 15:53:51 +0800

    第一周作业: 100W求素数

--- mytest/第一周作業/求100萬以內的素數.py
+++ mytest/第一周作業/求100萬以內的素數.py
@@ -1,7 +1,22 @@
+import time
+from functools import wraps
+
 num = 1000000
-# for i in range(10):
-#     if (1 // i) == 0 and (i // i) == 0:
-#         print(i)
 for i in range(2,num):
-    if (i % 2 ) == 0:
+    for a in range(2,i-1):
+        if i % a == 0:
+            break
+    else:
         print(i)
+
+
+# def test2():
+#     num = 1000000
+#     for i in range(2,num):
+#         if i == 2 or i == 3:
+#             print(i)
+#         elif i % 2 == 0 or i % 3 == 0:
+#             continue
+#         else:
+#             print(i)
+# test2()
