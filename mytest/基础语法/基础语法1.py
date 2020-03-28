cef66313abeada63d25f78fdc246c9d0e536729f
Administrator <282913448@qq.com>
2020-03-28 23:06:43 +0800

    同步笔记2020年3月28日23时06分

---
+++ mytest/基础语法/基础语法1.py
@@ -0,0 +1,8 @@
+print('1+1={0}'.format(1+1))
+print('1-1={0}'.format(1-1))
+print('2x2={0}'.format(2*2))
+print('3/2={0}'.format(3/2))
+print('5//2={0}'.format(5//2))
+print('5**2={0}'.format(5**2))
+print('8%4={0}'.format(8%4))
+
